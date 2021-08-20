5b97802120cab4fa24bb61215781f528eaddd1e8
Alaribe <laribright8@gmail.com>
2021-08-20 04:54:32 -0700

    Complete python snake game

---
+++ scoreboard.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.high_score = 0
+        self.score = 0
+        self.penup()
+        self.goto(0, 260)
+        self.update_scoreboard()
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.write(f"score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
